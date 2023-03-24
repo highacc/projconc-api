a2f4e61a37dee7c69dce57a8cc95cc2ddf635925
Olavo e Souto <olavo_souto@hotmail.com>
2023-03-24 12:46:03 -0300

    fix: Login auth bad endpoint 404

--- src/models/note.ts
+++ src/models/note.ts
@@ -1,9 +1,10 @@
 import { InferSchemaType, model, Schema } from "mongoose";
 
 const noteSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, required: true },
     title: { type: String, required: true },
     text: { type: String },
-}, {timestamps: true});
+}, { timestamps: true });
 
 type Note = InferSchemaType<typeof noteSchema>;
 
